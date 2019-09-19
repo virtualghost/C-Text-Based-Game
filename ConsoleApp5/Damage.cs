f2cd844a7b60e4fa9bbbfd8cdb65f45cc5d617c9
dumitrescuandrei <catalinandreidumitrescu@gmail.com>
2019-09-20 00:22:33 +0300

    no message

---
+++ ConsoleApp5/Damage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Damage
+    {
+        public Dictionary<string, string> PhysicalAttacks;
+        public Dictionary<string, string> ChaosAttacks;
+        public Dictionary<string, string> LightAttacks;
+        public Damage()
+        {
+            this.PhysicalAttacks= new Dictionary<string, string>();
+            this.ChaosAttacks = new Dictionary<string, string>();
+            this.LightAttacks = new Dictionary<string, string>();
+            this.PhysicalAttacks.Add("FA", "FistAttack");
+            this.PhysicalAttacks.Add("KA", "KnifeAttack");
+            this.PhysicalAttacks.Add("PA", "PistolAttack");
+            this.ChaosAttacks.Add("CB", "ChaosBolt");
+            this.ChaosAttacks.Add("CC", "ChaosCurse");
+            //this.ChaosAttacks.Add("", "ShadowPanic");
+            //this.LightAttacks.Add("", "");
+            //this.LightAttacks.Add("", "");
+            //this.LightAttacks.Add("", "");
+            //this.SpellBook.Add("A1")
+        }
+    }
+}
---
+++ ConsoleApp5/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Enemy
+    {
+        public Dictionary<int, string> Enemies;
+        string[] EnemyDescriptions = new string[500];
+        public Enemy()
+        {
+            this.Enemies = new Dictionary<int, string>();
+            this.Enemies.Add(0, "Burglar");
+            /*this.Enemies.Add(2, "Burglar");
+            this.Enemies.Add(3, "Burglar");
+            this.Enemies.Add(4, "Burglar");
+            this.Enemies.Add(5, "Burglar");
+            this.Enemies.Add(6, "Burglar"); */
+        }
+        public void SetEnemyDescriptions()
+        {
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            /* EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie.";
+            EnemyDescriptions[0] = "A male human wearing a hoodie."; */
+        }
+
+    }
+}

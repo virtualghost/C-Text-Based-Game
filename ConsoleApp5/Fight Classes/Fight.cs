41d47ad862e9096659da1518a50871355844d11f
dumitrescuandrei <catalinandreidumitrescu@gmail.com>
2019-09-20 18:30:38 +0300

    no message

---
+++ ConsoleApp5/Fight Classes/Fight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Fight_Classes
+{
+    class Fight
+    {
+        public float Health;
+        public float Damage;
+
+    }
+}
---
+++ ConsoleApp5/Player Classes/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Dictionary<int, string> Items { get; set; }
+
+        public float Damage { get; set; }
+
+        public float Health { get; set; }
+
+        public Player(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Items = new Dictionary<int, string>();
+            this.Damage = 0;
+            this.Health = 20;
+
+        }
+
+    }
+}

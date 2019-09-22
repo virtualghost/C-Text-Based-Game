c4586eb383bc448a20d6b89a879c65ced9597fdc
dumitrescuandrei <catalinandreidumitrescu@gmail.com>
2019-09-22 16:51:01 +0300

    no message

---
+++ ConsoleApp5/Items/ItemsInitializers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextGame.Items;
+
+namespace TextGame.Items
+{
+    class ItemsInitializers
+    {
+        public BaseItems Wallet = new BaseItems("Wallet", "Money Storage", 100);
+    }
+}

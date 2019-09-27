e91077f61d5237ee4f89674eb93ac5eecdb559f4
dumitrescuandrei <catalinandreidumitrescu@gmail.com>
2019-09-28 00:43:58 +0300

    finished the first day

--- ConsoleApp5/StoryParts/StoryLauncher.cs
+++ ConsoleApp5/StoryParts/StoryLauncher.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TextGame.Characters;
-using TextGame.Items;
-using TextGame.Technicalities;
-using TextGame.Fighting;
 using TextGame.Enemies;
+using TextGame.Fighting;
+using TextGame.Items;
 using TextGame.Items.InventoryItems;
+using System.Text;
 
 namespace TextGame.StoryParts
 {
@@ -17,29 +14,29 @@ namespace TextGame.StoryParts
         static Player player;
         public static void Introduction()
         {
-                Console.WriteLine("The Warp - Alpha");
-                Console.WriteLine("Welcome to The Warp. You wake up one day and reality seems more distorted with each day that passes.");
-                Console.WriteLine("Type in your character's name: ");
-                string CharacterName = Console.ReadLine();
-                while (GeneralChecker.NameChecker(CharacterName))
-                {
-                    Errors.NameContainsNumbers();
-                    Console.WriteLine("Type in character's name: ");
-                    CharacterName = Console.ReadLine();
-                }
+            Console.WriteLine("The Warp - Alpha (Learning project, focused more on learning C# than on writing an interesting story)");
+            Console.WriteLine("Welcome to The Warp. You wake up one day and reality seems more distorted with each day that passes.");
+            Console.WriteLine("Type in your character's name: ");
+            string CharacterName = Console.ReadLine();
+            while (GeneralChecker.NameChecker(CharacterName))
+            {
+                Errors.NameContainsNumbers();
+                Console.WriteLine("Type in character's name: ");
+                CharacterName = Console.ReadLine();
+            }
+            Console.WriteLine("Type in your character's age: ");
+            uint CharacterAge = Convert.ToUInt32(Console.ReadLine());
+            while (GeneralChecker.AgeChecker(CharacterAge))
+            {
+                Errors.AgeIsTooOld();
                 Console.WriteLine("Type in your character's age: ");
-                uint CharacterAge = Convert.ToUInt32(Console.ReadLine());
-                while (GeneralChecker.AgeChecker(CharacterAge))
-                {
-                    Errors.AgeIsTooOld();
-                    Console.WriteLine("Type in your character's age: ");
-                    CharacterAge = Convert.ToUInt32(Console.ReadLine());
-                }
-                Console.WriteLine("Greetings, " + CharacterName);
-                player = new Player(CharacterName, CharacterAge);
-                Console.WriteLine("Press any key to continue, ESC ends the game.");
-                Console.WriteLine("Enemies are more susceptible to different types of attacks. Damage can be of differing types, such as Physical, Shadow or Light. To see a description of the current enemy, press D");
-                
+                CharacterAge = Convert.ToUInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Greetings, " + CharacterName);
+            player = new Player(CharacterName, CharacterAge);
+            Console.WriteLine("Press any key to continue, ESC ends the game.");
+            Console.WriteLine("Enemies are more susceptible to different types of attacks. Damage can be of differing types, such as Physical, Shadow or Light. To see a description of the current enemy, press D");
+
         }
         public static void FirstDay()
         {
@@ -60,7 +57,7 @@ namespace TextGame.StoryParts
                 Fight.OneVersusOne(player, EnemyInitializers.burglar);
             }
             else wallet.DeductValue(100);
-            
+
             Console.WriteLine($"Your wallet now has {wallet.Amount} dollars left.");
             /* var value = new BaseItems();
             if (player.MyInventory.Dictionary.TryGetValue("Wallet", out value))
@@ -72,9 +69,23 @@ namespace TextGame.StoryParts
             {
                 Console.WriteLine("Key = \"tif\" is not found.");
             } */
-                                      
+
             Console.WriteLine("Shocked from the experience, you rush to enter the metro. It is crowded, but fairly safe save for thieves. After a few stations and a bit of walking, you find yourself at work. \n" +
-                "You sit down at your desk and boot up your PC, however a feeling in the back of your minid lingers that everything is not as it seems.");
+                "You sit down at your desk and boot up your PC, however a feeling in the back of your mind lingers that everything is not as it seems.\n" +
+                "After the PC finally boots up (the company is on a budget), a new email notification shows up.");
+            var SuspiciousEmail = new StringBuilder(300);
+            SuspiciousEmail.AppendLine("Sender: umbrellacompany@fsb.su");
+            SuspiciousEmail.AppendLine("Title: The work must be done");
+            SuspiciousEmail.AppendLine("Content: It's time.");
+            Console.WriteLine(SuspiciousEmail.ToString());
+            Console.WriteLine("What a strange email, you think. It's most likely an attempt to spread malware, but you don't care because this company does not invest in a proper IT department anyway.");
+            Console.WriteLine("You quickly forget about this incident and move on to working on today's tasks.");
+            Console.WriteLine("Time passes and you need to go home. Same story, same routine, it's depressing but it pays the bills.");
+            Console.WriteLine("Once home, sleep comes quickly and you get to it.");
+        }
+        public static void SecondDay()
+        {
+
         }
     }
 }
